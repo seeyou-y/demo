e0bb5c8735f690f7effb3d9e954cf861aca25f7b
Zhang <bb5581731@163.com>
2019-09-08 09:54:40 +0800

    //zb add pro

--- src/main/java/com/example/controller/ProduceClotroller.java
+++ src/main/java/com/example/controller/ProduceClotroller.java
@@ -2,7 +2,7 @@ package com.example.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//zb add
 @RestController
 @RequestMapping("pro")
 public class ProduceClotroller {
